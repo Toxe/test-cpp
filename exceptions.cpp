c2017b4cd007634da043b8308312a7e4882a569f
Toxe <toxe@toxe.net>
2019-06-13 07:59:18 +0200

    exceptions.cpp

---
+++ exceptions.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdexcept>
+
+void generic_exception()
+{
+    try {
+        throw std::exception{};
+    } catch (std::exception& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
+}
+
+void exception_with_error_message()
+{
+    try {
+        throw std::runtime_error{"something happened"};
+    } catch (std::runtime_error& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
+}
+
+void catch_all()
+{
+try {
+    throw std::runtime_error{"catch all"};
+} catch (std::overflow_error& e) {
+    std::cout << "exception: " << e.what() << std::endl;
+} catch (std::range_error& e) {
+    std::cout << "exception: " << e.what() << std::endl;
+} catch (...) {
+    std::cout << "catch all" << std::endl;
+}
+}
+
+void rethrow()
+{
+    try {
+        try {
+            throw std::runtime_error{"rethrow"};
+        } catch (...) {
+            throw;
+        }
+    } catch (std::runtime_error& e) {
+        std::cout << "caught: " << e.what() << std::endl;
+    }
+}
+
+int main()
+{
+    generic_exception();
+    exception_with_error_message();
+    catch_all();
+    rethrow();
+}

85a46cfe5cddaa1c73b8070a041d7db85dd1c1cb
Toxe <toxe@toxe.net>
2019-07-08 15:15:35 +0200

    Compiler Warnungen aktiviert.

--- function_pointers.cpp
+++ function_pointers.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-int calc_length(const std::string &s1, const std::string &s2)
+std::string::size_type calc_length(const std::string &s1, const std::string &s2)
 {
     return s1.size() + s2.size();
 }
@@ -9,7 +9,7 @@ int calc_length(const std::string &s1, const std::string &s2)
 
 int main()
 {
-    int(*p1) (const std::string &, const std::string &);
+    std::string::size_type(*p1) (const std::string &, const std::string &);
     p1 = calc_length;
 
     decltype(calc_length) *p2 = calc_length;

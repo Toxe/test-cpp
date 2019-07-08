b332b3ba0afbffe314c9dfe87ac23af7803a958a
Toxe <toxe@toxe.net>
2019-07-08 09:30:02 +0200

    constructors.cpp erweitert.

--- constructors.cpp
+++ constructors.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 
 struct Point {
-    int x = 0;
-    int y = 0;
+    int x_ = 0;
+    int y_ = 0;
+    int z_ = 0;
 
     Point() = default;
-    Point(int defaultX, int defaultY) : x(defaultX), y(defaultY) { };
+    Point(int x, int y, int z) : x_(x), y_(y), z_{z} { };
+    // Point(int x, int y, int z) : z_(z), y_(y), x_{x} { };  // Warnung, falsche Reihenfolge
 
-    void print() { std::cout << "{" << x << "," << y << "}" << std::endl; };
+    void print() { std::cout << "{" << x_ << "," << y_ << "," << z_ << "}" << std::endl; };
 };
 
 
 int main()
 {
     Point p1;
-    Point p2(1, 2);
-    Point p3{ 3, 4 };
-    Point p4 = { 5, 6 };
+    Point p2(1, 2, 3);
+    Point p3{4, 5, 6};
+    Point p4 = {7, 8, 9};
     Point p5 = p4;
-    Point p6(p5);
+    Point p6(p4);
+    Point p7(p4);
 
     p1.print();
     p2.print();
@@ -26,6 +29,5 @@ int main()
     p4.print();
     p5.print();
     p6.print();
-
-    return 0;
+    p7.print();
 }

9130d006a6c824df937a779983b24912273dde5a
Toxe <toxe@toxe.net>
2020-03-05 11:17:01 +0100

    Update

--- initialization.cpp
+++ initialization.cpp
@@ -3,9 +3,20 @@
 
 int main()
 {
-	int x{5};
-	auto y{x};
+    // initialization with 0
+    int a = 0;
+    int b = {};
+    int c{};
 
-	std::vector<int> v1 = {1, 2, 3};
-	std::vector<int> v2{4, 5, 6};
+    // initialization with value
+    int d = 10;
+    int e = {10};
+    int f{10};
+    int g(10);
+
+    // short i1{1.0};      // error
+    // short i2{100'000};  // error
+
+    std::vector<int> v1 = {1, 2, 3};
+    std::vector<int> v2{4, 5, 6};
 }

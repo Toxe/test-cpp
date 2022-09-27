440a02490b64a045b55b7cefe09f8b83a305e013
Toxe <toxe@toxe.net>
2022-09-27 15:09:53 +0200

    Add array.cpp

---
+++ src/array.cpp
@@ -0,0 +1,10 @@
+#include <array>
+
+int main()
+{
+    std::array<int, 4> numbers1;
+    std::array<int, 4> numbers2{1, 2, 3, 4};
+    std::array<int, 4> numbers3 = {1, 2, 3, 4};
+
+    std::array numbers4{1, 2, 3, 4};
+}

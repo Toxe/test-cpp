33643804d091beb41dc703ca74141dc677284e2b
Toxe <toxe@toxe.net>
2022-10-14 15:25:49 +0200

    Update chrono_duration.cpp

--- src/chrono_duration.cpp
+++ src/chrono_duration.cpp
@@ -1,28 +1,54 @@
 #include <chrono>
 #include <iostream>
-#include <thread>
 
 using namespace std::literals::chrono_literals;
 
+void output_seconds(const std::chrono::seconds s)
+{
+    std::cout << s.count() << "s\n";
+}
+
+void output_milliseconds(const std::chrono::milliseconds ms)
+{
+    std::cout << ms.count() << "ms\n";
+}
+
+void initialization()
+{
+    int ten = 10;
+
+    // std::chrono::seconds s1 = 10;  // error: not implicitly constructible from int
+    std::chrono::seconds s2{ten};
+    std::chrono::seconds s3{10};
+    std::chrono::seconds s4 = 10s;
+    std::chrono::seconds s5 = s2 + s3 + s4;
+
+    output_seconds(s5);
+}
+
+void conversion()
+{
+    // output_seconds(1);  // error: not implicitly constructible from int
+    output_seconds(std::chrono::seconds{2});
+    output_seconds(3s);
+
+    output_milliseconds(std::chrono::seconds{2});
+    output_milliseconds(3s);
+}
+
+void mixed_mode()
+{
+    std::chrono::seconds s{3};
+    std::chrono::milliseconds ms{50};
+
+    auto sum = s + ms;
+
+    output_milliseconds(sum);
+}
+
 int main()
 {
-    std::chrono::minutes minutes{2};
-    std::chrono::seconds seconds{5};
-    auto sum = minutes + seconds;
-    std::cout << minutes.count() << "m + " << seconds.count() << "s = " << sum.count() << "s\n";
-
-    seconds = 5min;  // automatic cast of minutes --> seconds
-    // minutes = 300s;  // error, needs explicit duration_cast<>
-    minutes = std::chrono::duration_cast<std::chrono::minutes>(301s);
-    std::cout << minutes.count() << "m, " << seconds.count() << "s\n";
-
-    auto t0 = std::chrono::high_resolution_clock::now();  // returns most likely nanoseconds
-    std::this_thread::sleep_for(5ms);
-    auto t1 = std::chrono::high_resolution_clock::now();
-    auto us = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0);
-
-    std::cout << "slept for " << us.count() << "μs or " << (t1 - t0).count() << " (probably) ns\n";
-    std::cout << "std::chrono::high_resolution_clock period = "
-              << std::chrono::high_resolution_clock::period::num << ":"
-              << std::chrono::high_resolution_clock::period::den << '\n';
+    initialization();
+    conversion();
+    mixed_mode();
 }
